824f4319a945e9e8ceaa65c26c7acb0f36b484a7
Katsuya Iida <katsuya.iida@gmail.com>
2026-01-11 13:27:08 +0000

    other unicode fonts

--- mlfont/cli.py
+++ mlfont/cli.py
@@ -18,9 +18,11 @@ class Main:
     ) -> None:
         r"""Generate font with FontMaskModel."""
         from tqdm import tqdm
+        import torch
         from .fontmask import FontMaskDataModule, FontMaskModel
 
         data = FontMaskDataModule(
+            kernel=(2,2),
             batch_size=batch_size,
             download=True,
             data_type=data_type,
@@ -30,13 +32,17 @@ class Main:
 
         model = FontMaskModel.load_from_checkpoint(
             ckpt_path,
+            kernel=(2,2),
             timestep_skip=timestep_skip,
         )
         model.eval()
+        model_predict_step = torch.compile(
+            torch.no_grad(model.predict_step)
+        )
 
         with open(output, "w") as fp:
             for batch_idx, batch in tqdm(enumerate(data.predict_dataloader())):
-                outputs = model.predict_step(batch, batch_idx)
+                outputs = model_predict_step(batch, batch_idx)
                 charsets = outputs["charset"]
                 encodings = outputs["encoding"]
                 image = outputs["image"]
@@ -112,6 +118,58 @@ class Main:
                 font.chars.append(char)
         bdffont.save(font, output)
 
+    def makeshnmu(
+        self,
+        *,
+        input: str = "data/ShinonomeBDFFontDataset/raw/shinonome-0.9.11/bdf/",
+        output: str = "data/local",
+    ) -> None:
+        r"""Make Unicode Shinonome fonts."""
+
+        from . import bdffont
+        import os
+
+        files = [
+            "shnm6x12a.bdf",
+            "shnm7x14a.bdf",
+            "shnm8x16a.bdf",
+            "shnm9x18a.bdf",
+        ]
+        names = [
+            "-Shinonome-Gothic-Medium-R-Normal--12-110-75-75-C-60-ISO10646-1",
+            "-Shinonome-Gothic-Medium-R-Normal--14-130-75-75-C-70-ISO10646-1",
+            "-Shinonome-Gothic-Medium-R-Normal--16-150-75-75-C-80-ISO10646-1",
+            "-Shinonome-Gothic-Medium-R-Normal--18-170-75-75-C-90-ISO10646-1",
+        ]
+
+        M = {}
+        for cp in range(256):
+            if 0x80 <= cp < 0xa1 or 0xe0 <= cp < 0x100:
+                continue
+            if 0x00 <= cp < 0x80 and cp != 0x7e:
+                continue
+            if cp == 0x7e:
+                ch = chr(0x203e)
+            else:
+                ch = bytes([cp]).decode("Windows-31J")
+            M[cp] = ch
+            # print("%02x" % cp, "U+%04x" % ord(ch), ch)
+
+        for file, name in zip(files, names):
+            ufont = bdffont.load(os.path.join(input, file))
+            rfont = bdffont.load(os.path.join(input, file.replace("a.bdf", "r.bdf")))
+            ufont.font = name
+            ufont.properties["charset_registry"] = "ISO10646"
+            x = []
+            for ch in rfont.chars:
+                cp = ch.encoding
+                if cp in M:
+                    ch.name = "%04x" % ord(M[cp])
+                    ch.encoding = ord(M[cp])
+                    x.append(ch)
+                    ch.bitmap = rfont.tobytes(ch)
+                    ufont.chars.append(ch)
+            bdffont.save(ufont, os.path.join(output, file.replace("a.bdf", "u.bdf")))
 
 if __name__ == "__main__":
     auto_cli(Main)
